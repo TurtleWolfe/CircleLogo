741f65f0f6f4107a8c46a57fee99de10977ce7ae
TurtleWolfe <JonPohlner@gmail.com>
2020-12-11 19:54:05 -0500

    Rotating Logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './ScriptHammerCom.png';
-import hammer from './Wooden-hammer-01.png';
+// import logo from './ScriptHammerCom.png';
+// import hammer from './Wooden-hammer-01.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <img src={hammer} className="App-hammer" alt="wooden mallet" />
+        {/* <img src="https://crudgames.com/logo192.png" alt=""></img> */}
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <img src={hammer} className="App-hammer" alt="wooden mallet" /> */}
+        <span className="App-hammer">
+          <img src="https://crudgames.com/logo192.png" alt=""></img>
+        </span>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
